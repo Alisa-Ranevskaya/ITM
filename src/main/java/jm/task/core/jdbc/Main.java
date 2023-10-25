26573d97a19eacd98e114281e60342ba3491d115
Алиса Раневская <aliska_ranevskaya_2004@mail.ru>
2023-10-25 14:58:27 +0300

    1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+            userService.createUsersTable();
+            userService.saveUser("Amber", "AMB", (byte) 15);
+            userService.saveUser("Anna", "ANN", (byte) 34);
+            userService.saveUser("Eliza", "ALI", (byte) 45);
+            userService.saveUser("Katie", "KAT", (byte) 56);
+            userService.removeUserById(3);
+            userService.getAllUsers();
+            userService.cleanUsersTable();
+            userService.dropUsersTable();
+
     }
 }

0983799d67f6942c4b06cc045c3e1c0be7cfbbe8
Алиса Раневская <aliska_ranevskaya_2004@mail.ru>
2023-10-25 22:42:03 +0300

    Revert "1.1.3"
    This reverts commit 26573d97a19eacd98e114281e60342ba3491d115.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,44 +1,5 @@
 package jm.task.core.jdbc.util;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.Properties;
-
-
 public class Util {
     // реализуйте настройку соеденения с БД
-
-    private static Properties getAplication(){
-        Properties properties =new Properties();
-        try(InputStream in = Util.class.getClassLoader().getResourceAsStream("application.properties")){
-            properties.load(in);
-
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-        return properties;
-    }
-
-    public static Connection getconnect() {
-        Properties properties = getAplication();
-        Connection connect = null;
-        try {
-            connect = DriverManager.getConnection(properties.getProperty("jdbc:postgresql://localhost:5432/postgres"),
-                    properties.getProperty("postgres"),
-                    properties.getProperty("admin"));
-            if (connect != null) {
-                System.out.println("Connect");
-
-            } else {
-                System.out.println("Disconnect");
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        return  connect;
-    }
 }
-
-
